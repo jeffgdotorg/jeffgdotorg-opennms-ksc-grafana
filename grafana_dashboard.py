b5260199afa7f4ea9da47f306f3021ad245bcadd
Jeff Gehlbach <jeffg@opennms.com>
2020-12-24 09:49:38 -0500

    Fixed the TypeError by actually calling the function.

--- grafana_dashboard.py
+++ grafana_dashboard.py
@@ -4,7 +4,7 @@ import re
 
 def empty_dashboard():
   dash_dict = {}
-  dash_dict['__inputs'] = [ { 'name': 'DS_OPENNMS-PM', 'label': 'opennms-pm', 'description': '', 'type': 'datasource', pluginId: 'opennms-helm-performance-datasource', pluginName: 'OpenNMS Performance' } ]
+  dash_dict['__inputs'] = [ { 'name': 'DS_OPENNMS-PM', 'label': 'opennms-pm', 'description': '', 'type': 'datasource', 'pluginId': 'opennms-helm-performance-datasource', 'pluginName': 'OpenNMS Performance' } ]
   dash_dict['__requires'] = [ { 'type': 'grafana', 'id': 'grafana', 'name': 'Grafana', 'version': '6.7.5' }, { 'type': 'panel', 'id': 'graph', 'name': 'Graph', 'version': '' }, { 'type': 'datasource', 'id': 'opennms-helm-performance-datasource', 'name': 'OpenNMS Performance', 'version': '1.0.0' } ]
   dash_dict['editable'] = True
   dash_dict['gnetId'] = None
@@ -27,7 +27,7 @@ def empty_dashboard():
 def set_dashboard_title(dash_dict, title):
   dash_dict['title'] = title
 
-def graph_panel(title, description, left_y_label, resource, attributes):
+def graph_panel(panel_id, title, description, left_y_label, resource, attributes):
   ref_idx = 0
   resource_pat = re.compile('^(node.*?\[.*?\])\.(\w+\[.*?\])$')
   node_id = None
@@ -37,13 +37,13 @@ def graph_panel(title, description, left_y_label, resource, attributes):
     node_id = resource_mat.group(1)
     resource_id = resource_mat.group(2)
 
-  panel_dict = { 'aliasColors': {}, 'bars': False, 'dashLength': 10, 'dashes': False, 'datasource': '${DS_OPENNMS-PM}', 'description': description, 'fill': 1, 'fillGradient': 0, 'gridPos': { 'h': 9, 'w': 12, 'x': 0, 'y': 0 }, 'hiddenSeries': False, 'id': None, 'legend': {}, 'lines': True, 'lineWidth': 1, 'nullPointMode': 'null', 'options': { 'dataLinks': [] }, 'percentage': False, 'pointradius': 2, 'renderer': 'flot', 'seriesOverrides': [], 'spaceLength': 10, 'stack': False, 'steppedLine': False, 'targets': [], 'thresholds': [], 'timeFrom': None, 'timeRegions': [], 'timeShift': None, 'title': title, 'tooltip': { 'shared': True, 'sort': 0, 'value_type': 'individual' }, 'type': 'graph', 'xaxis': { 'buckets': None, 'mode': 'time', 'name': None, 'show': True, 'values': [] }, 'yaxes': [], 'yaxis': { 'align': False, 'alignLevel': None } }
+  panel_dict = { 'aliasColors': {}, 'bars': False, 'dashLength': 10, 'dashes': False, 'datasource': '${DS_OPENNMS-PM}', 'description': description, 'fill': 1, 'fillGradient': 0, 'gridPos': { 'h': 9, 'w': 12, 'x': 0, 'y': 0 }, 'hiddenSeries': False, 'id': panel_id, 'legend': {}, 'lines': True, 'lineWidth': 1, 'nullPointMode': 'null', 'options': { 'dataLinks': [] }, 'percentage': False, 'pointradius': 2, 'renderer': 'flot', 'seriesOverrides': [], 'spaceLength': 10, 'stack': False, 'steppedLine': False, 'targets': [], 'thresholds': [], 'timeFrom': None, 'timeRegions': [], 'timeShift': None, 'title': title, 'tooltip': { 'shared': True, 'sort': 0, 'value_type': 'individual' }, 'type': 'graph', 'xaxis': { 'buckets': None, 'mode': 'time', 'name': None, 'show': True, 'values': [] }, 'yaxes': [], 'yaxis': { 'align': False, 'alignLevel': None } }
 
   for bute in attributes:
     ref_id = 'A' + str(ref_idx)
+    panel_dict['targets'].append( { 'attribute': bute, 'label': '', 'nodeId': node_id, 'refId': ref_id, 'resourceId': resource_id, 'type': 'attribute' } )
+    panel_dict['yaxes'].append( { 'format': 'short', 'label': left_y_label if ref_id == 0 else '', 'logBase': 1, 'max': None, 'min': None, 'show': True } )
     ref_idx += 1
-    panel_dict['targets'].append( [ { 'attribute': bute, 'label': '', 'nodeId': node_id, 'refId': ref_id, 'resourceId': resource_id, 'type': 'attribute' } ] )
-    panel_dict['yaxes'].append( [ { 'format': 'short', 'label': left_y_label if ref_id == 0 else '', 'logBase': 1, 'max': None, 'min': None, 'show': True } ] )
 
   return panel_dict
 
--- test.py
+++ test.py
@@ -9,14 +9,16 @@ sg = snmp_graph.load_graphs("/Users/jeffg/git/horizon-work/opennms-base-assembly
 ksc = ksc_report.load_reports("/Users/jeffg/Documents/clients/USPTO/2020/20201222_ksc_report_to_grafana/ksc-performance-reports.xml")
 
 for report in ksc:
-  dash_dict = grafana_dashboard.empty_dashboard
-  #grafana_dashboard.set_dashboard_title(dash_dict, report.get('title'))
+  dash_dict = grafana_dashboard.empty_dashboard()
+  grafana_dashboard.set_dashboard_title(dash_dict, report.get('title'))
+  panel_id = 1
   for graph in report:
     title = snmp_graph.get_name(sg, graph.get("graphtype"))
     description = snmp_graph.get_command(sg, graph.get("graphtype"))
     left_y_label = snmp_graph.get_vertical_label(sg, graph.get("graphtype"))
     resource = graph.get("resource")
     attributes = snmp_graph.get_attributes(sg, graph.get("graphtype"))
-    new_panel = grafana_dashboard.graph_panel(title, description, left_y_label, resource, attributes)
+    new_panel = grafana_dashboard.graph_panel(panel_id, title, description, left_y_label, resource, attributes)
+    panel_id += 1
     grafana_dashboard.append_panel(dash_dict, new_panel)
     print(json.dumps(dash_dict))
